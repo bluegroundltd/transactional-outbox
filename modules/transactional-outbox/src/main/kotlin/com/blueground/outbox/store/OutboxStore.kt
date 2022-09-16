a14c151521d080bf72de2f73cc49d462009cf9bc
Apostolos Kiraleos <apostolos.kiraleos@theblueground.com>
2022-09-16 13:30:21 +0300

    feature(admin): OutboxStore implementation
    [VR-2366]

--- modules/transactional-outbox/src/main/kotlin/com/blueground/outbox/store/OutboxStore.kt
+++ modules/transactional-outbox/src/main/kotlin/com/blueground/outbox/store/OutboxStore.kt
@@ -3,9 +3,9 @@ package com.blueground.outbox.store
 import com.blueground.outbox.item.OutboxItem
 
 interface OutboxStore {
-  fun insert(outboxItem: OutboxItem)
+  fun insert(outboxItem: OutboxItem): OutboxItem
 
-  fun update(outboxItem: OutboxItem)
+  fun update(outboxItem: OutboxItem): OutboxItem?
 
   fun fetch(outboxFilter: OutboxFilter): List<OutboxItem>
 }
