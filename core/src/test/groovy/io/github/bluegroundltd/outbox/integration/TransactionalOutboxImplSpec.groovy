cdb99a7269c6a8f7910a84901bfa9b348164bfb9
Panayiotis Tritakis <panagiotis.tritakis@theblueground.com>
2024-07-17 17:59:31 +0300

    chore: remove erroneous `shutdown` unit tests
    Removes the `shutdown` related unit tests from
    `TransactionalOutboxImplSpec`:
    - Some them do not make sense, e.g. there are two items (which also
      happen to be the exact same instance) queued for processing, but
      then only one of them is reset to pending.
    - There is a separate suite `OutboxShutdownSpec` that provides a range
      of unit tests specifically for the `shutdown` method and covers all
      possible paths.
    
    [SW-587]

--- core/src/test/groovy/io/github/bluegroundltd/outbox/integration/TransactionalOutboxImplSpec.groovy
+++ core/src/test/groovy/io/github/bluegroundltd/outbox/integration/TransactionalOutboxImplSpec.groovy
@@ -6,14 +6,10 @@ import io.github.bluegroundltd.outbox.TransactionalOutbox
 import io.github.bluegroundltd.outbox.TransactionalOutboxImpl
 import io.github.bluegroundltd.outbox.event.InstantOutboxPublisher
 import io.github.bluegroundltd.outbox.executor.FixedThreadPoolExecutorServiceFactory
-import io.github.bluegroundltd.outbox.item.OutboxItem
-import io.github.bluegroundltd.outbox.item.OutboxStatus
 import io.github.bluegroundltd.outbox.item.OutboxType
 import io.github.bluegroundltd.outbox.item.factory.OutboxItemFactory
-import io.github.bluegroundltd.outbox.store.OutboxFilter
 import io.github.bluegroundltd.outbox.store.OutboxStore
 import io.github.bluegroundltd.outbox.utils.DummyHandler
-import io.github.bluegroundltd.outbox.utils.OutboxItemBuilder
 import spock.lang.Specification
 
 import java.time.Clock
@@ -50,95 +46,6 @@ class TransactionalOutboxImplSpec extends Specification {
       [],
       DURATION_ONE_NANO
     )
-  }
-
-  def "Should return when shutdown is called and the executor is empty"() {
-    when:
-      transactionalOutbox.shutdown()
-
-    then:
-      0 * _
-  }
-
-  def "Should force outbox shutdown when shutdown is called and the timeout elapsed before termination"() {
-    given:
-      def expectedOutboxItem =
-        OutboxItemBuilder.make().withType(type).build()
-
-    and:
-      def now = Instant.now(clock)
-
-    when:
-      transactionalOutbox.monitor()
-
-    then:
-      1 * monitorLocksProvider.acquire()
-      1 * store.fetch(_) >> { OutboxFilter filter ->
-        with(filter) {
-          outboxPendingFilter.nextRunLessThan == now
-          outboxRunningFilter.rerunAfterLessThan == now
-        }
-        [expectedOutboxItem]
-      }
-      1 * store.update(_) >> { OutboxItem item ->
-        with(item) {
-          it.status == OutboxStatus.RUNNING
-          it.lastExecution == now
-          it.rerunAfter == item.lastExecution + DURATION_ONE_HOUR
-        }
-        return item
-      }
-      1 * monitorLocksProvider.release()
-      0 * _
-
-    when:
-      transactionalOutbox.shutdown()
-
-    then:
-      0 * _
-  }
-
-  def "Should delegate to the outbox store when shutdown is called and there are tasks that didn't start execution"() {
-    given:
-      def pendingItem = OutboxItemBuilder.make().withType(type).build()
-      def items = [pendingItem, pendingItem]
-      def now = Instant.now(clock)
-
-    when:
-      transactionalOutbox.monitor()
-
-    then:
-      1 * monitorLocksProvider.acquire()
-      1 * store.fetch(_) >> { OutboxFilter filter ->
-        with(filter) {
-          outboxPendingFilter.nextRunLessThan == now
-        }
-        items
-      }
-      items.size() * store.update(_) >> { OutboxItem item ->
-        with(item) {
-          it.status == OutboxStatus.RUNNING
-          it.lastExecution == now
-          it.rerunAfter == item.lastExecution + DURATION_ONE_HOUR
-        }
-        return item
-      }
-      1 * monitorLocksProvider.release()
-      0 * _
-
-    when:
-      transactionalOutbox.shutdown()
-
-    then:
-      1 * store.update(_) >> { OutboxItem item ->
-        with(item) {
-          it.status == OutboxStatus.PENDING
-          it.nextRun == now
-          it.rerunAfter == null
-        }
-        return item
-      }
-      0 * _
   }
 
   def "Should delegate to outbox store when `cleanup` is invoked"() {
